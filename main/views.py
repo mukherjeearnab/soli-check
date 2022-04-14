ff468062369ecf16259ddec650acf0a0eacceba4
Arnab Mukherjee <mukherjeearnab905@gmail.com>
2022-04-14 16:05:23 +0530

    Furnished form with dummy results

--- main/views.py
+++ main/views.py
@@ -9,7 +9,13 @@ def index(response):
     if response.method == 'POST':
         form = CheckBytecode(response.POST)
         # check bytecode
-        pass
+        if form.is_valid():
+            bytecode = form.cleaned_data['bytecode']
+            if bytecode == 'hello':
+                result = True
+            else:
+                result = False
     else:
+        result = None
         form = CheckBytecode()
-    return render(response, 'main/check.html', {'form': form})
+    return render(response, 'main/check.html', {'form': form, 'result': result})
