2c28f07a3e0299bd229a0ea1c9d92a7d26a037fd
Arnab Mukherjee <mukherjeearnab905@gmail.com>
2022-04-15 22:27:32 +0530

    Prepared Model and Prepare scripts, tested model prediciton

--- main/ml_model/model.py
+++ main/ml_model/model.py
@@ -1,5 +1,24 @@
+from tensorflow.keras.models import load_model
+import numpy as np
+
+
+from .prepare import prepare
+
+
 def check(bytecode):
+    opcode = prepare(bytecode)
+    detection(opcode)
     if bytecode == 'hello':
         return True
     else:
         return False
+
+
+def detection(vector):
+    model = load_model('./model/model_sm_rus_128.h5')
+
+    # print('PUSSY', np.isnan(vector).any())
+
+    result = model.predict(np.array(vector))
+
+    print('RES', result)
--- main/ml_model/prepare.py
+++ main/ml_model/prepare.py
@@ -1,6 +1,7 @@
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 import numpy as np
+import pickle
 
 OPCODE_SIZE = 150
 OPCODE_SEQ_LEN = 1800
@@ -20,8 +21,23 @@ def re_hex_val(opcode):
 
 
 def tokenizer(opcode):
+    with open('./model/tokenizer.pickle', 'rb') as fh:
+        tokenizer = pickle.load(fh)
+
     tokenized_opcodes = tokenizer.texts_to_sequences([opcode])
     padded_opcodes = pad_sequences(
         tokenized_opcodes, maxlen=OPCODE_SEQ_LEN, padding=PADDING_TYPE, truncating=TRUNC_TYPE)
 
     return np.array(padded_opcodes)
+
+
+def prepare(opcode):
+    # Remove Operands
+    opcode = re_hex_val(opcode)
+
+    # Tokenize the Opcode Sequence
+    opcode = tokenizer(opcode)
+
+    print(opcode)
+
+    return opcode
